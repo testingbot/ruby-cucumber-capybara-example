bfe65e273ebee084d77f4d255dbf46a19733d23c
jochen <jochen@netlog.com>
2022-07-22 13:32:40 +0200

    some changes to fix behaviour on newer versions

--- Rakefile
+++ Rakefile
@@ -2,22 +2,17 @@ def run_tests(platform, browser, version, junit_dir)
   system("platform=\"#{platform}\" browserName=\"#{browser}\" version=\"#{version}\" JUNIT_DIR=\"#{junit_dir}\" parallel_cucumber features -n 20")
 end
 
-task :windows_10_ie_11 do
-  run_tests('Windows 10', 'internet explorer', '11', 'junit_reports/windows_10_ie_11')
+task :windows_10_chrome_latest do
+  run_tests('Windows 10', 'chrome', 'latest', 'junit_reports/windows_10_chrome_latest')
 end
 
-task :windows_7_firefox_latest do
-  run_tests('Windows 7', 'firefox','latest', 'junit_reports/windows_7_firefox_latest')
-end
-
-task :os_x_10_14_chrome_latest do
-  run_tests('OS X 10.14', 'chrome', 'latest', 'junit_reports/os_x_10_14_chrome_latest')
+task :bigsur_chrome_latest do
+  run_tests('BIGSUR', 'chrome', 'latest', 'junit_reports/bigsur_chrome_latest')
 end
 
 multitask :test_testingbot => [
-    :windows_10_ie_11,
-    :windows_7_firefox_latest,
-    :os_x_10_14_chrome_latest,
+    :windows_10_chrome_latest,
+    :bigsur_chrome_latest,
   ] do
     puts 'Running automation'
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -6,23 +6,23 @@ Capybara.default_max_wait_time = 10
 Capybara.default_driver = :selenium
 
 Before do | scenario |
-  jobname = "#{scenario.feature.name} - #{scenario.name}"
+  jobname = "#{scenario.name}"
 
   Capybara.register_driver :selenium do | app|
-    capabilities = {
-      :version => ENV['version'],
-      :browserName => ENV['browserName'],
-      :platform => ENV['platform'],
-      :name => jobname
-    }
+    capabilities = Selenium::WebDriver::Options.chrome
+    capabilities.browser_version = ENV['version']
+    capabilities.platform_name = ENV['platform']
+    tb_options = {}
+    tb_options[:name] = jobname
+    tb_options['selenium-version'] = '3.14.0'
+    capabilities.add_option('tb:options', tb_options)
     url = "https://#{ENV['TB_KEY']}:#{ENV['TB_SECRET']}@hub.testingbot.com/wd/hub".strip
 
     Capybara::Selenium::Driver.new(app,
                                    :browser => :remote, :url => url,
-                                   :desired_capabilities => capabilities)
+                                   :capabilities => capabilities)
   end
 
-  # Capybara.current_driver = :remote
   Capybara.session_name = "#{jobname} - #{ENV['platform']} - " +
     "#{ENV['browserName']} - #{ENV['version']}"
 
@@ -36,9 +36,9 @@ After do | scenario |
   @driver.quit
   api = TestingBot::Api.new(ENV['TB_KEY'], ENV['TB_SECRET'])
   if scenario.exception
-    api.update_test(sessionid, { :success => false })
+    api.update_test(@session_id, { :success => false })
   else
-    api.update_test(sessionid, { :success => true })
+    api.update_test(@session_id, { :success => true })
   end
   Capybara.use_default_driver
 end
